e14af9e8e775d1a8a3692701ba676e4151fc4e97
qoth-0 <bny1324@naver.com>
2023-11-17 17:49:48 +0900

    .

--- 1.DDL.sql
+++ 1.DDL.sql
@@ -3,14 +3,20 @@ CREATE DATABASE board;
 -- 데이터베이스 선택
 USE board;
 -- author 테이블 신규 생성
-CREATE TABLE author(id INT, name VARCHAR(255), 
-email VARCHAR(255), password VARCHAR(255), test1 VARCHAR(255) PRIMARY KEY(id));
+CREATE TABLE author(
+    id INT, name VARCHAR(100) not null, 
+    email VARCHAR(255), 
+    password VARCHAR(255), 
+    role VARCHAR(50), 
+    address VARCHAR(255), 
+    PRIMARY KEY(id));
+
 -- 테이블 목록 조회
 SHOW TABLES;
 -- 테이블 컬럼조회
 DESCRIBE author;
 -- host 테이블 신규 생성
-CREATE TABLE posts(id INT PRIMARY KEY, title VARCHAR(255), content VARCHAR(255), author_id INT, FOREIGN KEY(author_id) REFERENCES author(id))
+CREATE TABLE posts(id INT PRIMARY KEY, title VARCHAR(255), contents VARCHAR(3000), author_id INT, FOREIGN KEY(author_id) REFERENCES author(id));
 
 --  테이블 컬럼 상세 조회
 SHOW FULL COLUMNS FROM author;
